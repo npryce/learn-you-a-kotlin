d6be1e1d59500796c4e4009e8e434c1d68cfbc19
Duncan McGregor <duncan.mcgregor@rbs.com>
2019-09-05 11:22:14 +0100

    JIRA-9999 did a conversion, now wait 30 mins

--- exercise/learnyouakotlin/part1/SessionTests.kt
+++ exercise/learnyouakotlin/part1/SessionTests.kt
@@ -1,40 +1,39 @@
-package learnyouakotlin.part1;
+package learnyouakotlin.part1
 
-import org.junit.Test;
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import java.util.Arrays.asList
 
-import static java.util.Arrays.asList;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
+class SessionTests {
 
-public class SessionTests {
-
-    Session original = new Session("The Title", null, new Slots(1, 2), new Presenter("Alice"));
+    private val original = Session("The Title", null, Slots(1, 2), Presenter("Alice"))
 
     @Test
-    public void can_change_presenters() {
+    fun `can change presenters`() {
         assertThat(
-            original.withPresenters(asList(new Presenter("Bob"), new Presenter("Carol"))),
-            equalTo(new Session("The Title", null, new Slots(1, 2), new Presenter("Bob"), new Presenter("Carol"))));
+            original.withPresenters(asList(Presenter("Bob"), Presenter("Carol"))),
+            equalTo(Session("The Title", null, Slots(1, 2), Presenter("Bob"), Presenter("Carol"))))
     }
 
     @Test
-    public void can_change_title() {
+    fun `can change title`() {
         assertThat(
             original.withTitle("Another Title"),
-            equalTo(new Session("Another Title", null, new Slots(1, 2), new Presenter("Alice"))));
+            equalTo(Session("Another Title", null, Slots(1, 2), Presenter("Alice"))))
     }
 
     @Test
-    public void can_change_subtitle() {
+    fun `can change subtitle`() {
         assertThat(
             original.withSubtitle("The Subtitle"),
-            equalTo(new Session("The Title", "The Subtitle", new Slots(1, 2), new Presenter("Alice"))));
+            equalTo(Session("The Title", "The Subtitle", Slots(1, 2), Presenter("Alice"))))
     }
 
     @Test
-    public void can_remove_subtitle() {
+    fun `can remove subtitle`() {
         assertThat(
             original.withSubtitle("The Subtitle").withSubtitle(null),
-            equalTo(new Session("The Title", null, new Slots(1, 2), new Presenter("Alice"))));
+            equalTo(Session("The Title", null, Slots(1, 2), Presenter("Alice"))))
     }
 }
