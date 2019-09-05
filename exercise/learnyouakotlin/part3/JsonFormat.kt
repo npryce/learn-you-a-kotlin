bed9affddb44a9e5d0e93acfde9812d5fb0cfe1e
Duncan McGregor <duncan.mcgregor@rbs.com>
2019-09-05 14:18:32 +0100

    JIRA-999999 Further transform of JsonFormat

--- exercise/learnyouakotlin/part3/JsonFormat.kt
+++ exercise/learnyouakotlin/part3/JsonFormat.kt
@@ -6,9 +6,6 @@ import learnyouakotlin.part1.Presenter
 import learnyouakotlin.part1.Session
 import learnyouakotlin.part1.Slots
 import learnyouakotlin.part3.Json.*
-import java.util.*
-import java.util.stream.Collectors
-import java.util.stream.StreamSupport.stream
 
 object JsonFormat {
 
@@ -20,25 +17,18 @@ object JsonFormat {
                 prop("first", session.slots.start),
                 prop("last", session.slots.endInclusive)
             )),
-            prop("presenters", array(session.presenters, { presenterToJson(it) })))
+            prop("presenters", array(session.presenters) { presenterToJson(it) }))
     }
 
-    @Throws(JsonMappingException::class)
     fun sessionFromJson(json: JsonNode): Session {
         val title = nonBlankText(json.path("title"))
         val subtitle = optionalNonBlankText(json.path("subtitle"))
 
         val authorsNode = json.path("presenters")
-        val presenters = stream(spliterator(Iterable { authorsNode.elements() }), false)
-            .map{ presenterFromJson(it) }
-            .collect(Collectors.toList())
+        val presenters = authorsNode.map { presenterFromJson(it) }
         val slots = Slots(json.at("/slots/first").intValue(), json.at("/slots/last").intValue())
 
         return Session(title!!, subtitle, slots, presenters)
-    }
-
-    private fun spliterator(elements: Iterable<JsonNode>): Spliterator<JsonNode> {
-        return elements.spliterator()
     }
 
     private fun presenterToJson(p: Presenter): JsonNode {
@@ -49,7 +39,6 @@ object JsonFormat {
         return Presenter(authorNode.path("name").asText())
     }
 
-    @Throws(JsonMappingException::class)
     private fun optionalNonBlankText(node: JsonNode): String? {
         return if (node.isMissingNode) {
             null
@@ -58,7 +47,6 @@ object JsonFormat {
         }
     }
 
-    @Throws(JsonMappingException::class)
     private fun nonBlankText(node: JsonNode): String? {
         val text = node.asText()
         return if (node.isNull || text == "") {
