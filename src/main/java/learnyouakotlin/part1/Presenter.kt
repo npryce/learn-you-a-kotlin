c88d9705b8d2e7f01e811b43a8f80709e48834ba
Duncan McGregor <duncan.mcgregor@rbs.com>
2019-12-04 09:52:38 +0100

    Convert Presenter to data class

--- src/main/java/learnyouakotlin/part1/Presenter.kt
+++ src/main/java/learnyouakotlin/part1/Presenter.kt
@@ -1,21 +1,3 @@
 package learnyouakotlin.part1
 
-import java.util.*
-
-class Presenter(val name: String) {
-    override fun equals(o: Any?): Boolean {
-        if (this === o) return true
-        if (o == null || javaClass != o.javaClass) return false
-        val presenter = o as Presenter
-        return name == presenter.name
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(name)
-    }
-
-    override fun toString(): String {
-        return name
-    }
-
-}
+data class Presenter(val name: String)
