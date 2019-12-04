409134e463dbd3c258de775b533b13b502ca5594
Duncan McGregor <duncan.mcgregor@rbs.com>
2019-12-04 10:16:24 +0100

    Convert Session to data class

--- src/main/java/learnyouakotlin/part1/Session.kt
+++ src/main/java/learnyouakotlin/part1/Session.kt
@@ -3,7 +3,7 @@ package learnyouakotlin.part1
 import java.util.*
 
 
-class Session(
+data class Session(
     val title: String,
     val subtitle: String?,
     val slots: Slots,
@@ -16,29 +16,6 @@ class Session(
         slots: Slots,
         vararg presenters: Presenter
     ) : this(title, subtitle, slots, listOf<Presenter>(*presenters))
-
-    override fun equals(o: Any?): Boolean {
-        if (this === o) return true
-        if (o == null || javaClass != o.javaClass) return false
-        val session = o as Session?
-        return title == session!!.title &&
-            subtitle == session.subtitle &&
-            slots == session.slots &&
-            presenters == session.presenters
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(title, subtitle, slots, presenters)
-    }
-
-    override fun toString(): String {
-        return "Session{" +
-            "title='" + title + '\''.toString() +
-            ", subtitle='" + subtitle + '\''.toString() +
-            ", slots=" + slots +
-            ", presenters=" + presenters +
-            '}'.toString()
-    }
 
     fun withPresenters(newLineUp: List<Presenter>): Session {
         return Session(title, subtitle, slots, newLineUp)
