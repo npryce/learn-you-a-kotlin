657ebd044635d6aff0f8f8278a87c23eaf41f04b
Duncan McGregor <duncan.mcgregor@rbs.com>
2019-12-04 11:47:50 +0100

    Make Sessions Kotlin

--- src/main/java/learnyouakotlin/part2/Sessions.kt
+++ src/main/java/learnyouakotlin/part2/Sessions.kt
@@ -1,28 +1,18 @@
-package learnyouakotlin.part2;
+package learnyouakotlin.part2
 
-import learnyouakotlin.part1.Session;
+import learnyouakotlin.part1.Session
 
-import javax.annotation.Nullable;
-import java.util.List;
+fun subtitleOf(session: Session?): String? {
+    return session?.subtitle
+}
 
-class Sessions {
+fun subtitleOrPrompt(session: Session): String {
+    return session.subtitle ?: "click to enter subtitle"
+}
 
-    public static @Nullable String subtitleOf(@Nullable Session session) {
-        if (session == null)
-            return null;
-        else
-            return session.getSubtitle();
-    }
-
-    public static String subtitleOrPrompt(Session session) {
-        if (session.getSubtitle() == null)
-            return "click to enter subtitle";
-        else
-            return session.getSubtitle();
-    }
-
-    public static @Nullable Session findWithTitle(List<Session> sessions, String title) {
-        return sessions.stream().filter(session -> session.getTitle().equalsIgnoreCase(title)).findFirst().orElse(null);
-    }
-
+fun findWithTitle(sessions: List<Session>, title: String): Session? {
+    return sessions.stream()
+        .filter { (title1) -> title1.equals(title, ignoreCase = true) }
+        .findFirst()
+        .orElse(null)
 }
