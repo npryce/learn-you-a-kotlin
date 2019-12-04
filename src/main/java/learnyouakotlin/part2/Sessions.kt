e7c9b44c52efa020d10eb3b3b441fea4f0bf5e5f
Duncan McGregor <duncan.mcgregor@rbs.com>
2019-12-04 12:15:39 +0100

    Gets rid of stream()

--- src/main/java/learnyouakotlin/part2/Sessions.kt
+++ src/main/java/learnyouakotlin/part2/Sessions.kt
@@ -6,8 +6,5 @@ val Session.subtitleOrPrompt: String
     get() = this.subtitle ?: "click to enter subtitle"
 
 fun findWithTitle(sessions: List<Session>, title: String): Session? {
-    return sessions.stream()
-        .filter { (title1) -> title1.equals(title, ignoreCase = true) }
-        .findFirst()
-        .orElse(null)
+    return sessions.firstOrNull { (title1) -> title1.equals(title, ignoreCase = true) }
 }
