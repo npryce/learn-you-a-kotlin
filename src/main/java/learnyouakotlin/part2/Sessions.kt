2eef0e9951b80134fd2beb08f28f6dd47c290d7a
Nat Pryce <sw@natpryce.com>
2022-11-11 14:46:43 +0000

    Type Alias for Sessions

--- src/main/java/learnyouakotlin/part2/Sessions.kt
+++ src/main/java/learnyouakotlin/part2/Sessions.kt
@@ -5,5 +5,7 @@ import learnyouakotlin.part1.Session
 val Session.subtitleOrPrompt: String
     get() = subtitle ?: "click to enter subtitle"
 
-fun List<Session>.find(title: String): Session? =
+typealias Sessions = List<Session>
+
+fun Sessions.find(title: String): Session? =
     firstOrNull { it.title.equals(title, ignoreCase = true) }
