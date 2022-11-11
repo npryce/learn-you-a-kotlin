2c9dfe69d1726f1399af9203112a46f70ac95565
Nat Pryce <sw@natpryce.com>
2022-11-11 15:24:30 +0000

    Converting JsonFormatTests to kotlin

--- src/test/java/learnyouakotlin/part3/JsonFormatTests.kt
+++ src/test/java/learnyouakotlin/part3/JsonFormatTests.kt
@@ -1,77 +1,69 @@
-package learnyouakotlin.part3;
+package learnyouakotlin.part3
 
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.oneeyedmen.okeydoke.junit.ApprovalsRule;
-import learnyouakotlin.part1.Presenter;
-import learnyouakotlin.part1.Session;
-import learnyouakotlin.part1.Slots;
-import org.junit.Rule;
-import org.junit.Test;
+import com.fasterxml.jackson.databind.JsonMappingException
+import com.oneeyedmen.okeydoke.junit.ApprovalsRule
+import learnyouakotlin.part1.Presenter
+import learnyouakotlin.part1.Session
+import learnyouakotlin.part1.Slots
+import learnyouakotlin.part3.Json.toStableJsonString
+import org.hamcrest.MatcherAssert
+import org.hamcrest.core.IsEqual
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
 
-import java.io.IOException;
-
-import static learnyouakotlin.part3.JsonFormat.sessionFromJson;
-import static learnyouakotlin.part3.JsonFormat.sessionToJson;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.fail;
-
-public class JsonFormatTests {
+class JsonFormatTests {
     @Rule
-    public final ApprovalsRule approval = ApprovalsRule.usualRule();
-
+    @JvmField
+    val approval: ApprovalsRule = ApprovalsRule.usualRule()
     @Test
-    public void session_to_json() {
-        Session session = new Session(
-                "Learn You a Kotlin For All The Good It Will Do You",
-                null,
-                new Slots(1, 2),
-                new Presenter("Duncan McGregor"),
-                new Presenter("Nat Pryce"));
-
-        JsonNode json = sessionToJson(session);
-        approval.assertApproved(Json.toStableJsonString(json));
+    fun session_to_json() {
+        val session = Session(
+            "Learn You a Kotlin For All The Good It Will Do You",
+            null,
+            Slots(1, 2),
+            Presenter("Duncan McGregor"),
+            Presenter("Nat Pryce")
+        )
+        val json = JsonFormat.sessionToJson(session)
+        approval.assertApproved(toStableJsonString(json))
     }
-
+    
     @Test
-    public void session_with_subtitle_to_json() {
-        Session session = new Session(
-                "Scrapheap Challenge",
-                "A Workshop in Postmodern Programming",
-                new Slots(3, 3),
-                new Presenter("Ivan Moore"));
-
-        JsonNode json = sessionToJson(session);
-        approval.assertApproved(Json.toStableJsonString(json));
+    fun session_with_subtitle_to_json() {
+        val session = Session(
+            "Scrapheap Challenge",
+            "A Workshop in Postmodern Programming",
+            Slots(3, 3),
+            Presenter("Ivan Moore")
+        )
+        val json = JsonFormat.sessionToJson(session)
+        approval.assertApproved(toStableJsonString(json))
     }
-
+    
     @Test
-    public void session_to_and_from_json() throws JsonMappingException {
-        Session original = new Session(
-                "Working Effectively with Legacy Tests",
-                null,
-                new Slots(4, 5),
-                new Presenter("Nat Pryce"),
-                new Presenter("Duncan McGregor"));
-
-        Session parsed = sessionFromJson(sessionToJson(original));
-        assertThat(parsed, equalTo(original));
+    fun session_to_and_from_json() {
+        val original = Session(
+            "Working Effectively with Legacy Tests",
+            null,
+            Slots(4, 5),
+            Presenter("Nat Pryce"),
+            Presenter("Duncan McGregor")
+        )
+        val parsed = JsonFormat.sessionFromJson(JsonFormat.sessionToJson(original))
+        MatcherAssert.assertThat(parsed, IsEqual.equalTo(original))
     }
-
+    
     @Test
-    public void reading_throws_with_blank_subtitle() throws IOException {
-        String json = ("{" +
-            "  'title' : 'Has blank subtitle'," +
-            "  'subtitle' : ''," +
-            "  'slots' : { 'first' : 3, 'last' : 3  }," +
-            "  'presenters' : [ {    'name' : 'Ivan Moore'  } ]\n" +
-            "}").replace("'", "\"");
+    fun reading_throws_with_blank_subtitle() {
+        val json =
+            """{  'title' : 'Has blank subtitle',  'subtitle' : '',  'slots' : { 'first' : 3, 'last' : 3  },  'presenters' : [ {    'name' : 'Ivan Moore'  } ]
+}""".replace("'", "\"")
         try {
-            sessionFromJson(Json.stableMapper.readTree(json));
-            fail();
-        } catch (JsonMappingException expected) {
-            assertThat(expected.getMessage(), equalTo("missing or empty text"));
+            JsonFormat.sessionFromJson(Json.stableMapper.readTree(json))
+            Assert.fail()
+        } catch (expected: JsonMappingException) {
+            MatcherAssert.assertThat(expected.message, IsEqual.equalTo("missing or empty text"))
         }
     }
 }
