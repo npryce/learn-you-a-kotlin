97e13ded4134cffa89e0e6edf1f87956494a6392
Nat Pryce <sw@natpryce.com>
2023-02-03 10:47:34 +0100

    converted to data class

--- src/main/java/learnyouakotlin/part1/Session.kt
+++ src/main/java/learnyouakotlin/part1/Session.kt
@@ -1,17 +1,13 @@
 package learnyouakotlin.part1
 
 import java.util.Arrays
-import java.util.Collections
-import java.util.Objects
 
-class Session(@JvmField val title: String, @JvmField val subtitle: String?, @JvmField val slots: Slots, presenters: List<Presenter>?) {
-    @JvmField
-    val presenters: List<Presenter>
-    
-    init {
-        this.presenters = Collections.unmodifiableList(ArrayList(presenters))
-    }
-    
+data class Session(
+    @JvmField val title: String,
+    @JvmField val subtitle: String?,
+    @JvmField val slots: Slots,
+    @JvmField val presenters: List<Presenter>
+) {
     constructor(title: String, subtitle: String?, slots: Slots, vararg presenters: Presenter?) : this(
         title,
         subtitle,
@@ -19,27 +15,7 @@ class Session(@JvmField val title: String, @JvmField val subtitle: String?, @Jvm
         Arrays.asList<Presenter>(*presenters)
     )
     
-    override fun equals(o: Any?): Boolean {
-        if (this === o) return true
-        if (o == null || javaClass != o.javaClass) return false
-        val session = o as Session
-        return title == session.title && subtitle == session.subtitle && slots == session.slots && presenters == session.presenters
-    }
-    
-    override fun hashCode(): Int {
-        return Objects.hash(title, subtitle, slots, presenters)
-    }
-    
-    override fun toString(): String {
-        return "Session{" +
-            "title='" + title + '\'' +
-            ", subtitle='" + subtitle + '\'' +
-            ", slots=" + slots +
-            ", presenters=" + presenters +
-            '}'
-    }
-    
-    fun withPresenters(newLineUp: List<Presenter>?): Session {
+    fun withPresenters(newLineUp: List<Presenter>): Session {
         return Session(title, subtitle, slots, newLineUp)
     }
     
