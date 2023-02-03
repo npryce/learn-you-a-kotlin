3b271bab567d43dd62aba6ba44293f10f11ba3ab
Nat Pryce <sw@natpryce.com>
2023-02-03 11:52:38 +0100

    Converted Slots.java to Slots.kt

--- src/main/java/learnyouakotlin/part1/Slots.kt
+++ src/main/java/learnyouakotlin/part1/Slots.kt
@@ -1,34 +1,3 @@
-package learnyouakotlin.part1;
+package learnyouakotlin.part1
 
-import java.util.Objects;
-
-public class Slots {
-    public final int start, endInclusive;
-
-    public Slots(int start, int endInclusive) {
-        this.start = start;
-        this.endInclusive = endInclusive;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Slots slots = (Slots) o;
-        return start == slots.start &&
-                endInclusive == slots.endInclusive;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(start, endInclusive);
-    }
-
-    @Override
-    public String toString() {
-        return "Slots{" +
-                "start=" + start +
-                ", endInclusive=" + endInclusive +
-                '}';
-    }
-}
+data class Slots(@JvmField val start: Int, @JvmField val endInclusive: Int)
