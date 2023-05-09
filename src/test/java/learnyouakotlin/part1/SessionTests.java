5c1580458ed5e231657416fe7cab6a44fc11dcef
Nat Pryce <sw@natpryce.com>
2023-05-09 08:25:26 +0100

    Refactor - convert test to JUnit 5

--- src/test/java/learnyouakotlin/part1/SessionTests.java
+++ src/test/java/learnyouakotlin/part1/SessionTests.java
@@ -1,10 +1,9 @@
 package learnyouakotlin.part1;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static java.util.Arrays.asList;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SessionTests {
 
@@ -12,29 +11,29 @@ public class SessionTests {
 
     @Test
     public void can_change_presenters() {
-        assertThat(
-            original.withPresenters(asList(new Presenter("Bob"), new Presenter("Carol"))),
-            equalTo(new Session("The Title", null, new Slots(1, 2), new Presenter("Bob"), new Presenter("Carol"))));
+        assertEquals(
+            new Session("The Title", null, new Slots(1, 2), new Presenter("Bob"), new Presenter("Carol")),
+            original.withPresenters(asList(new Presenter("Bob"), new Presenter("Carol"))));
     }
 
     @Test
     public void can_change_title() {
-        assertThat(
-            original.withTitle("Another Title"),
-            equalTo(new Session("Another Title", null, new Slots(1, 2), new Presenter("Alice"))));
+        assertEquals(
+            new Session("Another Title", null, new Slots(1, 2), new Presenter("Alice")),
+            original.withTitle("Another Title"));
     }
 
     @Test
     public void can_change_subtitle() {
-        assertThat(
-            original.withSubtitle("The Subtitle"),
-            equalTo(new Session("The Title", "The Subtitle", new Slots(1, 2), new Presenter("Alice"))));
+        assertEquals(
+            new Session("The Title", "The Subtitle", new Slots(1, 2), new Presenter("Alice")),
+            original.withSubtitle("The Subtitle"));
     }
 
     @Test
     public void can_remove_subtitle() {
-        assertThat(
-            original.withSubtitle("The Subtitle").withSubtitle(null),
-            equalTo(new Session("The Title", null, new Slots(1, 2), new Presenter("Alice"))));
+        assertEquals(
+            new Session("The Title", null, new Slots(1, 2), new Presenter("Alice")),
+            original.withSubtitle("The Subtitle").withSubtitle(null));
     }
 }
