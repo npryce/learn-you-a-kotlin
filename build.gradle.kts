1298129cefa2310241783dea0a312ee8cb936d88
Nat Pryce <sw@natpryce.com>
2023-05-09 09:29:37 +0100

    Refactor - remove JUnit 4 from the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ dependencies {
     api("com.google.code.findbugs:jsr305:3.0.2")
     api("com.fasterxml.jackson.core:jackson-databind:2.15.0")
 
-    testImplementation("junit:junit:4.13.2")
     testImplementation(platform("org.junit:junit-bom:5.9.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("com.natpryce:hamkrest:1.8.0.1")
