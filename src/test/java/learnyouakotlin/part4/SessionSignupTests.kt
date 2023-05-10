87f1f0bc645d460cf9e897eb100156fa76763460
Nat Pryce <sw@natpryce.com>
2023-05-10 23:43:30 +0100

    Temporary step: test treats SessionSignup as if it was immutable (apart from changing capacity)

--- src/test/java/learnyouakotlin/part4/SessionSignupTests.kt
+++ src/test/java/learnyouakotlin/part4/SessionSignupTests.kt
@@ -8,124 +8,124 @@ import kotlin.test.assertTrue
 class SessionSignupTests {
     @Test
     fun collects_signups() {
-        val signup = SessionSignup(capacity = 15)
+        var signup = SessionSignup(capacity = 15)
         
         assertEquals(emptySet(), signup.signups)
-        signup.signUp(alice)
+        signup = signup.signUp(alice)
         
         assertEquals(setOf(alice), signup.signups)
-        signup.signUp(bob)
+        signup = signup.signUp(bob)
         
         assertEquals(setOf(alice, bob), signup.signups)
-        signup.signUp(carol)
+        signup = signup.signUp(carol)
         
         assertEquals(setOf(alice, bob, carol), signup.signups)
-        signup.signUp(dave)
+        signup = signup.signUp(dave)
         
         assertEquals(setOf(alice, bob, carol, dave), signup.signups)
     }
     
     @Test
     fun each_attendee_can_only_sign_up_once() {
-        val signup = SessionSignup(capacity = 3)
+        var signup = SessionSignup(capacity = 3)
         
-        signup.signUp(alice)
-        signup.signUp(alice)
-        signup.signUp(alice)
+        signup = signup.signUp(alice)
+        signup = signup.signUp(alice)
+        signup = signup.signUp(alice)
         assertTrue(!signup.isFull)
         assertEquals(setOf(alice), signup.signups)
     }
     
     @Test
     fun can_cancel_signup() {
-        val signup = SessionSignup(capacity = 15)
+        var signup = SessionSignup(capacity = 15)
         
-        signup.signUp(alice)
-        signup.signUp(bob)
-        signup.signUp(carol)
-        signup.cancelSignUp(bob)
+        signup = signup.signUp(alice)
+        signup = signup.signUp(bob)
+        signup = signup.signUp(carol)
+        signup = signup.cancelSignUp(bob)
         assertEquals(setOf(alice, carol), signup.signups)
     }
     
     @Test
     fun can_only_sign_up_to_capacity() {
-        val signup = SessionSignup(capacity = 3)
+        var signup = SessionSignup(capacity = 3)
         
         assertTrue(!signup.isFull)
-        signup.signUp(alice)
+        signup = signup.signUp(alice)
         assertTrue(!signup.isFull)
-        signup.signUp(bob)
+        signup = signup.signUp(bob)
         assertTrue(!signup.isFull)
-        signup.signUp(carol)
+        signup = signup.signUp(carol)
         assertTrue(signup.isFull)
         assertFailsWith<IllegalStateException> { signup.signUp(dave) }
     }
     
     @Test
     fun duplicate_signup_ignored_when_full() {
-        val signup = SessionSignup(capacity = 3)
+        var signup = SessionSignup(capacity = 3)
         
-        signup.signUp(alice)
-        signup.signUp(bob)
-        signup.signUp(carol)
-        signup.signUp(alice) // does not throw
+        signup = signup.signUp(alice)
+        signup = signup.signUp(bob)
+        signup = signup.signUp(carol)
+        signup = signup.signUp(alice) // does not throw
     }
     
     @Test
     fun cannot_sign_up_after_session_has_started() {
-        val signup = SessionSignup(capacity = 3)
+        var signup = SessionSignup(capacity = 3)
         
-        signup.signUp(alice)
-        signup.signUp(bob)
+        signup = signup.signUp(alice)
+        signup = signup.signUp(bob)
         assertTrue(!signup.isSessionStarted)
-        signup.start()
+        signup = signup.start()
         assertTrue(signup.isSessionStarted)
         assertFailsWith<IllegalStateException> { signup.signUp(carol) }
     }
     
     @Test
     fun ignores_duplicate_signup_after_session_has_started() {
-        val signup = SessionSignup(capacity = 3)
+        var signup = SessionSignup(capacity = 3)
         
-        signup.signUp(alice)
-        signup.start()
-        signup.signUp(alice) // Does not throw
+        signup = signup.signUp(alice)
+        signup = signup.start()
+        signup = signup.signUp(alice) // Does not throw
     }
     
     @Test
     fun can_increase_capacity() {
-        val signup = SessionSignup(capacity = 2)
+        var signup = SessionSignup(capacity = 2)
         
-        signup.signUp(alice)
-        signup.signUp(bob)
+        signup = signup.signUp(alice)
+        signup = signup.signUp(bob)
         assertTrue(signup.isFull)
         
         signup.capacity = 4
         assertTrue(!signup.isFull)
-        signup.signUp(carol)
-        signup.signUp(dave)
+        signup = signup.signUp(carol)
+        signup = signup.signUp(dave)
         assertEquals(4, signup.capacity)
         assertTrue(signup.isFull)
     }
     
     @Test
     fun cannot_reduce_capacity_to_fewer_than_number_of_signups() {
-        val signup = SessionSignup(capacity = 4)
+        var signup = SessionSignup(capacity = 4)
         
-        signup.signUp(alice)
-        signup.signUp(bob)
-        signup.signUp(carol)
-        signup.signUp(dave)
+        signup = signup.signUp(alice)
+        signup = signup.signUp(bob)
+        signup = signup.signUp(carol)
+        signup = signup.signUp(dave)
         assertFailsWith<IllegalStateException> { signup.capacity = 3 }
     }
     
     @Test
     fun cannot_reduce_capacity_after_session_started() {
-        val signup = SessionSignup(capacity = 3)
-        signup.signUp(alice)
-        signup.signUp(bob)
-        signup.signUp(carol)
-        signup.start()
+        var signup = SessionSignup(capacity = 3)
+        signup = signup.signUp(alice)
+        signup = signup.signUp(bob)
+        signup = signup.signUp(carol)
+        signup = signup.start()
         assertFailsWith<IllegalStateException> { signup.capacity = 6 }
     }
     
