ec7ccaa146d7cd45ca5fb16ba6461e10baf5ed33
Nat Pryce <sw@natpryce.com>
2023-05-10 23:00:20 +0100

    Add kotlin and kotlin-test-junit5 to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    kotlin("jvm") version "1.8.21"
 }
 
 repositories {
@@ -10,12 +11,18 @@ dependencies {
     api("com.google.code.findbugs:jsr305:3.0.2")
     api("com.fasterxml.jackson.core:jackson-databind:2.15.0")
 
+    testImplementation(kotlin("test-junit5"))
     testImplementation(platform("org.junit:junit-bom:5.9.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("com.natpryce:hamkrest:1.8.0.1")
     testImplementation("com.oneeyedmen:okeydoke:1.3.3")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
 }
