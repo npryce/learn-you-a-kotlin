321a2b8e353f91c9d74ea93d2a4b5b6ac2a73d12
Nat Pryce <sw@natpryce.com>
2023-05-11 00:08:12 +0100

    Make it a data class and use the copy method to remove duplication and make code less error prone

--- src/main/java/learnyouakotlin/part4/SessionSignup.kt
+++ src/main/java/learnyouakotlin/part4/SessionSignup.kt
@@ -1,6 +1,6 @@
 package learnyouakotlin.part4
 
-class SessionSignup(
+data class SessionSignup(
     val capacity: Int = 0,
     val signups: Set<AttendeeId> = emptySet(),
     val isSessionStarted: Boolean = false
@@ -15,7 +15,7 @@ class SessionSignup(
         check(!isSessionStarted) {
             "you cannot change the capacity after the session as started"
         }
-        return SessionSignup(newCapacity, signups, isSessionStarted)
+        return copy(capacity = newCapacity)
     }
     
     val isFull: Boolean
@@ -26,13 +26,13 @@ class SessionSignup(
         else -> {
             check(!isSessionStarted) { "cannot sign up for session after it has started" }
             check(!isFull) { "session is full" }
-            SessionSignup(capacity, signups + attendeeId, isSessionStarted)
+            copy(signups = signups + attendeeId)
         }
     }
     
     fun cancelSignUp(attendeeId: AttendeeId) =
-        SessionSignup(capacity, signups - attendeeId, isSessionStarted)
+        copy(signups = signups - attendeeId)
     
     fun start() =
-        SessionSignup(capacity, signups, isSessionStarted = true)
+        copy(isSessionStarted = true)
 }
