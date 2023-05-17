592329a6c2c3bbca8a2213532d64686ee9eee96c
Nat Pryce <sw@natpryce.com>
2023-05-18 00:39:08 +0100

    Move capacity check to init block

--- src/main/java/learnyouakotlin/part4/SignupSheet.kt
+++ src/main/java/learnyouakotlin/part4/SignupSheet.kt
@@ -6,6 +6,11 @@ data class SignupSheet @JvmOverloads constructor(
     val isSessionStarted: Boolean = false,
     val signups: Set<AttendeeId> = emptySet()
 ) {
+    init {
+        check(signups.size <= capacity) {
+            "cannot have more sign-ups than capacity"
+        }
+    }
     
     val isFull: Boolean
         get() = signups.size == capacity
@@ -18,13 +23,11 @@ data class SignupSheet @JvmOverloads constructor(
     
     fun signUp(attendeeId: AttendeeId): SignupSheet {
         check(!isSessionStarted) { "you cannot change sign-ups for a session after it has started" }
-        check(!isFull) { "session is full" }
         return copy(signups = signups + attendeeId)
     }
     
     fun cancelSignUp(attendeeId: AttendeeId): SignupSheet {
         check(!isSessionStarted) { "you cannot change sign-ups for a session after it has started" }
-        
         return copy(signups = signups - attendeeId)
     }
 }
