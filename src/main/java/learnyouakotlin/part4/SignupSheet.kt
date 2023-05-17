412bf3d8c735b771611bcc8c2a1a0f043f453a77
Nat Pryce <sw@natpryce.com>
2023-05-17 09:54:08 +0100

    Make signup sheet *look* functional

--- src/main/java/learnyouakotlin/part4/SignupSheet.kt
+++ src/main/java/learnyouakotlin/part4/SignupSheet.kt
@@ -25,21 +25,24 @@ class SignupSheet {
     val isFull: Boolean
         get() = signups.size == capacity
     
-    fun sessionStarted() {
+    fun sessionStarted(): SignupSheet {
         isSessionStarted = true
+        return this
     }
     
     fun isSignedUp(attendeeId: AttendeeId): Boolean {
         return signups.contains(attendeeId)
     }
     
-    fun signUp(attendeeId: AttendeeId) {
+    fun signUp(attendeeId: AttendeeId): SignupSheet {
         check(!isSessionStarted) { "you cannot sign up for session after it has started" }
         check(!isFull) { "session is full" }
         signups = signups + attendeeId
+        return this
     }
     
-    fun cancelSignUp(attendeeId: AttendeeId) {
+    fun cancelSignUp(attendeeId: AttendeeId): SignupSheet {
         signups = signups - attendeeId
+        return this
     }
 }
