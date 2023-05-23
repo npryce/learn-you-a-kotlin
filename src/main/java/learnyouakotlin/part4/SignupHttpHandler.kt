2d9e9a775761f0edc0911a3f6710af3ae9ecfb7f
Nat Pryce <sw@natpryce.com>
2023-05-23 08:16:32 +0100

    Clean http handler

--- src/main/java/learnyouakotlin/part4/SignupHttpHandler.kt
+++ src/main/java/learnyouakotlin/part4/SignupHttpHandler.kt
@@ -1,144 +1,128 @@
-package learnyouakotlin.part4;
+package learnyouakotlin.part4
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import jakarta.ws.rs.core.Response;
-import org.glassfish.jersey.uri.UriTemplate;
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpHandler
+import jakarta.ws.rs.HttpMethod
+import jakarta.ws.rs.core.Response.Status
+import jakarta.ws.rs.core.Response.Status.CONFLICT
+import jakarta.ws.rs.core.Response.Status.METHOD_NOT_ALLOWED
+import jakarta.ws.rs.core.Response.Status.NOT_FOUND
+import jakarta.ws.rs.core.Response.Status.OK
+import org.glassfish.jersey.uri.UriTemplate
+import java.io.OutputStreamWriter
 
-import javax.annotation.Nullable;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static jakarta.ws.rs.HttpMethod.*;
-import static jakarta.ws.rs.core.Response.Status.*;
-
-
-public class SignupHttpHandler implements HttpHandler {
-    public static final UriTemplate signupsRoute =
-        new UriTemplate("/sessions/{sessionId}/signups");
-    public static final UriTemplate signupRoute =
-        new UriTemplate("/sessions/{sessionId}/signups/{attendeeId}");
-    public static final UriTemplate startedRoute =
-        new UriTemplate("/sessions/{sessionId}/started");
-
-    private static final List<UriTemplate> routes =
-        List.of(signupsRoute, signupRoute, startedRoute);
-
-
-    private final Transactor<SignupBook> transactor;
-
-    public SignupHttpHandler(Transactor<SignupBook> transactor) {
-        this.transactor = transactor;
-    }
-
-    @Override
-    public void handle(HttpExchange exchange) throws IOException {
-        final var params = new HashMap<String, String>();
-
-        final var matchedRoute = matchRoute(exchange, params);
-        if (matchedRoute == null) {
-            sendResponse(exchange, NOT_FOUND, "resource not found");
-            return;
-        }
-
-        transactor.perform(book -> {
-            final var sheet = book.sheetFor(SessionId.of(params.get("sessionId")));
-            if (sheet == null) {
-                sendResponse(exchange, NOT_FOUND, "session not found");
-                return;
+class SignupHttpHandler(private val transactor: Transactor<SignupBook>) : HttpHandler {
+    override fun handle(exchange: HttpExchange) {
+        val params = HashMap<String, String>()
+        val matchedRoute = routes
+            .firstOrNull { it.match(exchange.requestURI.path, params) }
+            ?: run {
+                sendResponse(exchange, NOT_FOUND, "resource not found")
+                return
             }
-
-            if (matchedRoute == signupsRoute) {
-                handleSignups(exchange, sheet);
-            } else if (matchedRoute == signupRoute) {
-                handleSignup(exchange, book, sheet, AttendeeId.of(params.get("attendeeId")));
-            } else if (matchedRoute == startedRoute) {
-                handleStarted(exchange, book, sheet);
-            }
-        });
-    }
-
-    private void handleSignups(HttpExchange exchange, SignupSheet sheet) throws IOException {
-        switch (exchange.getRequestMethod()) {
-            case GET -> {
-                sendResponse(exchange, OK,
-                    sheet.getSignups().stream()
-                        .map(Identifier::getValue)
-                        .collect(Collectors.joining("\n")));
-            }
-            default -> {
-                sendMethodNotAllowed(exchange);
+        
+        transactor.perform { book: SignupBook ->
+            val sheet = book.sheetFor(SessionId.of(params["sessionId"]))
+                ?: run {
+                    sendResponse(exchange, NOT_FOUND, "session not found")
+                    return@perform
+                }
+            
+            when (matchedRoute) {
+                signupsRoute -> handleSignups(exchange, sheet)
+                signupRoute -> handleSignup(exchange, book, sheet, AttendeeId.of(params["attendeeId"]))
+                startedRoute -> handleStarted(exchange, book, sheet)
             }
         }
     }
-
-    private void handleSignup(HttpExchange exchange, SignupBook book, SignupSheet sheet, AttendeeId attendeeId) throws IOException {
-        switch (exchange.getRequestMethod()) {
-            case GET -> {
-                sendResponse(exchange, OK, sheet.isSignedUp(attendeeId));
+    
+    private fun handleSignups(exchange: HttpExchange, sheet: SignupSheet) {
+        when (exchange.requestMethod) {
+            HttpMethod.GET -> {
+                sendResponse(exchange, OK, sheet.signups.joinToString("\n") { it.value })
             }
-            case POST -> {
+            
+            else -> {
+                sendMethodNotAllowed(exchange)
+            }
+        }
+    }
+    
+    private fun handleSignup(exchange: HttpExchange, book: SignupBook, sheet: SignupSheet, attendeeId: AttendeeId) {
+        when (exchange.requestMethod) {
+            HttpMethod.GET -> {
+                sendResponse(exchange, OK, sheet.isSignedUp(attendeeId))
+            }
+            
+            HttpMethod.POST -> {
                 try {
-                    sheet.signUp(attendeeId);
-                    book.save(sheet);
-                    sendResponse(exchange, OK, "subscribed");
-                } catch (IllegalStateException e) {
-                    sendResponse(exchange, CONFLICT, e.getMessage());
+                    sheet.signUp(attendeeId)
+                    book.save(sheet)
+                    sendResponse(exchange, OK, "subscribed")
+                } catch (e: IllegalStateException) {
+                    sendResponse(exchange, CONFLICT, e.message)
                 }
             }
-            case DELETE -> {
+            
+            HttpMethod.DELETE -> {
                 try {
-                    sheet.cancelSignUp(attendeeId);
-                    book.save(sheet);
-                    sendResponse(exchange, OK, "unsubscribed");
-                } catch (IllegalStateException e) {
-                    sendResponse(exchange, CONFLICT, e.getMessage());
+                    sheet.cancelSignUp(attendeeId)
+                    book.save(sheet)
+                    sendResponse(exchange, OK, "unsubscribed")
+                } catch (e: IllegalStateException) {
+                    sendResponse(exchange, CONFLICT, e.message)
                 }
             }
-            default -> {
-                sendMethodNotAllowed(exchange);
+            
+            else -> {
+                sendMethodNotAllowed(exchange)
             }
         }
     }
-
-    private void handleStarted(HttpExchange exchange, SignupBook book, SignupSheet sheet) throws IOException {
-        switch (exchange.getRequestMethod()) {
-            case GET -> {
-                sendResponse(exchange, OK, sheet.isSessionStarted());
+    
+    private fun handleStarted(exchange: HttpExchange, book: SignupBook, sheet: SignupSheet) {
+        when (exchange.requestMethod) {
+            HttpMethod.GET -> {
+                sendResponse(exchange, OK, sheet.isSessionStarted)
             }
-            case POST -> {
-                sheet.sessionStarted();
-                book.save(sheet);
-                sendResponse(exchange, OK, "started");
+            
+            HttpMethod.POST -> {
+                sheet.sessionStarted()
+                book.save(sheet)
+                sendResponse(exchange, OK, "started")
             }
-            default -> {
-                sendMethodNotAllowed(exchange);
-            }
-        }
-    }
-
-    private static @Nullable UriTemplate matchRoute(HttpExchange exchange, HashMap<String, String> paramsOut) {
-        for (final var t : routes) {
-            if (t.match(exchange.getRequestURI().getPath(), paramsOut)) {
-                return t;
+            
+            else -> {
+                sendMethodNotAllowed(exchange)
             }
         }
-        return null;
     }
-
-    private static void sendResponse(HttpExchange exchange, Response.Status status, Object bodyValue) throws IOException {
-        exchange.getResponseHeaders().add("Content-Type", "text/plain");
-        exchange.sendResponseHeaders(status.getStatusCode(), 0);
-        final var body = new OutputStreamWriter(exchange.getResponseBody());
-        body.write(bodyValue.toString());
-        body.flush();
-    }
-
-    private static void sendMethodNotAllowed(HttpExchange exchange) throws IOException {
-        sendResponse(exchange, METHOD_NOT_ALLOWED,
-            exchange.getRequestMethod() + " method not allowed");
+    
+    companion object {
+        @JvmField
+        val signupsRoute = UriTemplate("/sessions/{sessionId}/signups")
+        
+        @JvmField
+        val signupRoute = UriTemplate("/sessions/{sessionId}/signups/{attendeeId}")
+        
+        @JvmField
+        val startedRoute = UriTemplate("/sessions/{sessionId}/started")
+        
+        private val routes = listOf(signupsRoute, signupRoute, startedRoute)
+        
+        private fun sendResponse(exchange: HttpExchange, status: Status, bodyValue: Any?) {
+            exchange.responseHeaders.add("Content-Type", "text/plain")
+            exchange.sendResponseHeaders(status.statusCode, 0)
+            val body = OutputStreamWriter(exchange.responseBody)
+            body.write(bodyValue.toString())
+            body.flush()
+        }
+        
+        private fun sendMethodNotAllowed(exchange: HttpExchange) {
+            sendResponse(
+                exchange, METHOD_NOT_ALLOWED,
+                exchange.requestMethod + " method not allowed"
+            )
+        }
     }
 }
