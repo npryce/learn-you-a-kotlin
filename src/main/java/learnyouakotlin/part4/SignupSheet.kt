54e9a4580af19dfc6f5717f6a75d279f8078e57c
Nat Pryce <sw@natpryce.com>
2023-05-24 07:19:12 +0100

    Make immutable

--- src/main/java/learnyouakotlin/part4/SignupSheet.kt
+++ src/main/java/learnyouakotlin/part4/SignupSheet.kt
@@ -1,12 +1,12 @@
 package learnyouakotlin.part4
 
-class SignupSheet(val sessionId: SessionId, val capacity: Int) {
-    var signups: Set<AttendeeId> = emptySet()
-        private set
-    
-    var isSessionStarted = false
-        private set
-    
+class SignupSheet
+    @JvmOverloads constructor(
+    val sessionId: SessionId,
+    val capacity: Int,
+    val signups: Set<AttendeeId> = emptySet(),
+    val isSessionStarted: Boolean = false
+) {
     val isFull: Boolean
         get() = signups.size == capacity
     
@@ -17,18 +17,15 @@ class SignupSheet(val sessionId: SessionId, val capacity: Int) {
     fun signUp(attendeeId: AttendeeId): SignupSheet {
         check(!isSessionStarted) { "you cannot change sign-ups for a session after it has started" }
         check(!isFull) { "session is full" }
-        signups = signups + attendeeId
-        return this
+        return SignupSheet(sessionId, capacity, signups + attendeeId, isSessionStarted)
     }
     
     fun cancelSignUp(attendeeId: AttendeeId): SignupSheet {
         check(!isSessionStarted) { "you cannot change sign-ups for a session after it has started" }
-        signups = signups - attendeeId
-        return this
+        return SignupSheet(sessionId, capacity, signups - attendeeId, isSessionStarted)
     }
     
     fun sessionStarted(): SignupSheet {
-        isSessionStarted = true
-        return this
+        return SignupSheet(sessionId, capacity, signups, true)
     }
 }
