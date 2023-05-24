e1d7230a01d6016c213c46b23c121c53de2cd88f
Nat Pryce <sw@natpryce.com>
2023-05-24 07:56:43 +0100

    session full check moved to init

--- src/main/java/learnyouakotlin/part4/SignupSheet.kt
+++ src/main/java/learnyouakotlin/part4/SignupSheet.kt
@@ -1,21 +1,20 @@
 package learnyouakotlin.part4
 
-class SignupSheet
-    @JvmOverloads constructor(
+class SignupSheet @JvmOverloads constructor(
     val sessionId: SessionId,
     val capacity: Int,
     val signups: Set<AttendeeId> = emptySet(),
     val isSessionStarted: Boolean = false
 ) {
-    val isFull: Boolean
-        get() = signups.size == capacity
+    init {
+        check(signups.size <= capacity) { "session is full" }
+    }
     
     fun isSignedUp(attendeeId: AttendeeId): Boolean =
         signups.contains(attendeeId)
     
     fun signUp(attendeeId: AttendeeId): SignupSheet {
         check(!isSessionStarted) { "you cannot change sign-ups for a session after it has started" }
-        check(!isFull) { "session is full" }
         return SignupSheet(sessionId, capacity, signups + attendeeId, isSessionStarted)
     }
     
