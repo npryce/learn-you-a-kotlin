b30a4b8d402a02dca4c28a9209716a02ee2dc5ef
Nat Pryce <sw@natpryce.com>
2023-05-25 19:03:43 +0100

    Chainable

--- src/main/java/learnyouakotlin/part4/SignupSheet.kt
+++ src/main/java/learnyouakotlin/part4/SignupSheet.kt
@@ -25,22 +25,25 @@ class SignupSheet {
     val isFull: Boolean
         get() = signups.size == capacity
     
-    fun close() {
+    fun close(): SignupSheet {
         isClosed = true
+        return this
     }
     
     fun isSignedUp(attendeeId: AttendeeId): Boolean {
         return signups.contains(attendeeId)
     }
     
-    fun signUp(attendeeId: AttendeeId) {
+    fun signUp(attendeeId: AttendeeId): SignupSheet {
         check(!isClosed) { "sign-up has closed" }
         check(!isFull) { "session is full" }
         signups += attendeeId
+        return this
     }
     
-    fun cancelSignUp(attendeeId: AttendeeId) {
+    fun cancelSignUp(attendeeId: AttendeeId): SignupSheet {
         check(!isClosed) { "sign-up has closed" }
         signups -= attendeeId
+        return this
     }
 }
