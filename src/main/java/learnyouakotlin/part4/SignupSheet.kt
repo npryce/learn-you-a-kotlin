69bced934ebc95b9dab464696f972c3e66a9c6b5
Nat Pryce <sw@natpryce.com>
2023-05-25 19:11:00 +0100

    Move sessionId and capacity to constructor and make fixed

--- src/main/java/learnyouakotlin/part4/SignupSheet.kt
+++ src/main/java/learnyouakotlin/part4/SignupSheet.kt
@@ -1,24 +1,11 @@
 package learnyouakotlin.part4
 
-class SignupSheet {
-    var sessionId: SessionId? = null
-    
-    var capacity = 0
-        set(value) {
-            check(capacity == 0) { "you cannot change the capacity after it has been set" }
-            field = value
-        }
-    
+class SignupSheet(val sessionId: SessionId, val capacity: Int) {
     var signups = emptySet<AttendeeId>()
         private set
     
     var isClosed = false
         private set
-    
-    constructor(sessionId: SessionId?, capacity: Int) {
-        this.sessionId = sessionId
-        this.capacity = capacity
-    }
     
     val isFull: Boolean
         get() = signups.size == capacity
