a550d2c8c498cc78705232de2e90693e676ad9df
Nat Pryce <sw@natpryce.com>
2023-05-26 09:04:52 +0100

    use copy method

--- src/main/java/learnyouakotlin/part4/SignupSheet.kt
+++ src/main/java/learnyouakotlin/part4/SignupSheet.kt
@@ -14,18 +14,18 @@ data class SignupSheet(
         this(sessionId, capacity, emptySet(), false)
     
     fun close(): SignupSheet =
-        SignupSheet(sessionId, capacity, signups, true)
+        copy(isClosed = true)
     
     fun isSignedUp(attendeeId: AttendeeId): Boolean =
         signups.contains(attendeeId)
     
     fun signUp(attendeeId: AttendeeId): SignupSheet {
         check(!isClosed) { "sign-up has closed" }
-        return SignupSheet(sessionId, capacity, signups + attendeeId, false)
+        return copy(signups = signups + attendeeId)
     }
     
     fun cancelSignUp(attendeeId: AttendeeId): SignupSheet {
         check(!isClosed) { "sign-up has closed" }
-        return SignupSheet(sessionId, capacity, signups - attendeeId, false)
+        return copy(signups = signups - attendeeId)
     }
 }
