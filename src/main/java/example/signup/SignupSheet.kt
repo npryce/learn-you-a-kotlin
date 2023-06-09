2aad7bc1813d0aa0f877dc98f2a0c775131b7ac5
Nat Pryce <sw@natpryce.com>
2023-06-09 15:25:26 +0100

    Use Kotlin syntactic sugar for sets

--- src/main/java/example/signup/SignupSheet.kt
+++ src/main/java/example/signup/SignupSheet.kt
@@ -1,29 +1,29 @@
 package example.signup
 
-class SignupSheet private constructor(val sessionId: SessionId, val capacity: Int) {
+class SignupSheet(val sessionId: SessionId, val capacity: Int) {
     private val signups = LinkedHashSet<AttendeeId>()
+    
+    fun getSignups(): Set<AttendeeId> =
+        signups.toSet()
+    
     var isClosed = false
         private set
+    
     val isFull: Boolean
         get() = signups.size == capacity
     
-    fun getSignups(): Set<AttendeeId> {
-        return java.util.Set.copyOf(signups)
-    }
-    
-    fun isSignedUp(attendeeId: AttendeeId): Boolean {
-        return signups.contains(attendeeId)
-    }
+    fun isSignedUp(attendeeId: AttendeeId): Boolean =
+        attendeeId in signups
     
     fun signUp(attendeeId: AttendeeId) {
         check(!isClosed) { "sign-up has closed" }
         check(!isFull) { "session is full" }
-        signups.add(attendeeId)
+        signups += attendeeId
     }
     
     fun cancelSignUp(attendeeId: AttendeeId) {
         check(!isClosed) { "sign-up has closed" }
-        signups.remove(attendeeId)
+        signups -= attendeeId
     }
     
     fun close() {
