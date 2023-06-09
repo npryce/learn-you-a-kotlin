e8b2e51293c65a9bbdc929621f36fe2a6eea28b9
Nat Pryce <sw@natpryce.com>
2023-06-09 15:27:48 +0100

    Mutable reference to immutable set

--- src/main/java/example/signup/SignupSheet.kt
+++ src/main/java/example/signup/SignupSheet.kt
@@ -1,15 +1,13 @@
 package example.signup
 
 class SignupSheet(val sessionId: SessionId, val capacity: Int) {
-    private val signups = LinkedHashSet<AttendeeId>()
-    
-    fun getSignups(): Set<AttendeeId> =
-        signups.toSet()
+    var signups = emptySet<AttendeeId>()
+        private set
     
     var isClosed = false
         private set
     
-    val isFull: Boolean
+    private val isFull: Boolean
         get() = signups.size == capacity
     
     fun isSignedUp(attendeeId: AttendeeId): Boolean =
@@ -18,12 +16,12 @@ class SignupSheet(val sessionId: SessionId, val capacity: Int) {
     fun signUp(attendeeId: AttendeeId) {
         check(!isClosed) { "sign-up has closed" }
         check(!isFull) { "session is full" }
-        signups += attendeeId
+        signups = signups + attendeeId
     }
     
     fun cancelSignUp(attendeeId: AttendeeId) {
         check(!isClosed) { "sign-up has closed" }
-        signups -= attendeeId
+        signups = signups - attendeeId
     }
     
     fun close() {
