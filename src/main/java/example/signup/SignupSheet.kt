ec0cf488d2ad9b3ad363e8e4761dd004156a5806
Nat Pryce <sw@natpryce.com>
2023-06-13 11:13:32 +0100

    Java bean class to kotlin

--- src/main/java/example/signup/SignupSheet.kt
+++ src/main/java/example/signup/SignupSheet.kt
@@ -1,57 +1,32 @@
-package example.signup;
+package example.signup
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
-public class SignupSheet {
-    private final SessionId sessionId;
-    private final int capacity;
-    private final LinkedHashSet<AttendeeId> signups = new LinkedHashSet<>();
-    private boolean isClosed = false;
-
-    public SignupSheet(SessionId sessionId, int capacity) {
-        this.sessionId = sessionId;
-        this.capacity = capacity;
+class SignupSheet(val sessionId: SessionId, val capacity: Int) {
+    private val signups = LinkedHashSet<AttendeeId>()
+    var isClosed = false
+        private set
+    val isFull: Boolean
+        get() = signups.size == capacity
+    
+    fun getSignups(): Set<AttendeeId> {
+        return java.util.Set.copyOf(signups)
     }
-
-    public SessionId getSessionId() {
-        return sessionId;
+    
+    fun isSignedUp(attendeeId: AttendeeId): Boolean {
+        return signups.contains(attendeeId)
     }
-
-    public int getCapacity() {
-        return capacity;
+    
+    fun signUp(attendeeId: AttendeeId) {
+        check(!isClosed) { "sign-up has closed" }
+        check(!isFull) { "session is full" }
+        signups.add(attendeeId)
     }
-
-    public boolean isFull() {
-        return signups.size() == capacity;
+    
+    fun cancelSignUp(attendeeId: AttendeeId) {
+        check(!isClosed) { "sign-up has closed" }
+        signups.remove(attendeeId)
     }
-
-    public boolean isClosed() {
-        return isClosed;
-    }
-
-    public Set<AttendeeId> getSignups() {
-        return Set.copyOf(signups);
-    }
-
-    public boolean isSignedUp(AttendeeId attendeeId) {
-        return signups.contains(attendeeId);
-    }
-
-    public void signUp(AttendeeId attendeeId) {
-        if (isClosed()) throw new IllegalStateException("sign-up has closed");
-        if (isFull()) throw new IllegalStateException("session is full");
-
-        signups.add(attendeeId);
-    }
-
-    public void cancelSignUp(AttendeeId attendeeId) {
-        if (isClosed()) throw new IllegalStateException("sign-up has closed");
-
-        signups.remove(attendeeId);
-    }
-
-    public void close() {
-        isClosed = true;
+    
+    fun close() {
+        isClosed = true
     }
 }
