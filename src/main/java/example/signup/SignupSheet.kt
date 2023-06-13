67c2959520fdb8cdb9196660bb0931b45bbb811a
Nat Pryce <sw@natpryce.com>
2023-06-13 11:17:01 +0100

    Kotlin syntactic sugar for set operations

--- src/main/java/example/signup/SignupSheet.kt
+++ src/main/java/example/signup/SignupSheet.kt
@@ -1,29 +1,31 @@
 package example.signup
 
 class SignupSheet(val sessionId: SessionId, val capacity: Int) {
-    private val signups = LinkedHashSet<AttendeeId>()
+    private val signups = mutableSetOf<AttendeeId>()
+    
+    fun getSignups(): Set<AttendeeId> {
+        return signups.toSet()
+    }
+    
     var isClosed = false
         private set
+    
     val isFull: Boolean
         get() = signups.size == capacity
     
-    fun getSignups(): Set<AttendeeId> {
-        return java.util.Set.copyOf(signups)
-    }
-    
     fun isSignedUp(attendeeId: AttendeeId): Boolean {
-        return signups.contains(attendeeId)
+        return attendeeId in signups
     }
     
     fun signUp(attendeeId: AttendeeId) {
         check(!isClosed) { "sign-up has closed" }
         check(!isFull) { "session is full" }
-        signups.add(attendeeId)
+        signups += attendeeId
     }
     
     fun cancelSignUp(attendeeId: AttendeeId) {
         check(!isClosed) { "sign-up has closed" }
-        signups.remove(attendeeId)
+        signups -= attendeeId
     }
     
     fun close() {
